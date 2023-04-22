6229a26099f2db0a57d45e16c7e7230cea1e1081
iansw246 <ianjazz246@gmail.com>
2023-04-22 10:50:03 -0700

    Implement UserController

--- src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
+++ src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
@@ -1,26 +1,63 @@
 package com.shepherdmoney.interviewproject.controller;
 
+import com.shepherdmoney.interviewproject.model.User;
+import com.shepherdmoney.interviewproject.repository.UserRepository;
 import com.shepherdmoney.interviewproject.vo.request.CreateUserPayload;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 
 @RestController
 public class UserController {
 
-    // TODO: wire in the user repository (~ 1 line)
+    @Autowired
+    UserRepository repository;
 
+    /**
+     * Creates a user entity with the given information.
+     * @param payload Information with which to create user.
+     * @return 200 response with id of created user.
+     */
     @PutMapping("/user")
     public ResponseEntity<Integer> createUser(@RequestBody CreateUserPayload payload) {
-        // TODO: Create an user entity with information given in the payload, store it in the database
-        //       and return the id of the user in 200 OK response
-        return null;
+        User newUser = new User();
+        newUser.setName(payload.getName());
+        newUser.setEmail(payload.getEmail());
+        // New user has no credit cards initially
+        newUser.setCreditCardList(Collections.emptyList());
+
+        try {
+            User savedUser = repository.save(newUser);
+            return ResponseEntity.ok(savedUser.getId());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
+    /**
+     * Deletes the user with the given userId if exists.
+     * Returns 200 OK response of user exists and deletion was successful.
+     * Returns 400 Bad Request if user does not exist.
+     * Responses have message explaining response
+     * @param userId Id of user to delete.
+     * @return 200 response if deletion successful, 400 if user doesn't exist
+     */
     @DeleteMapping("/user")
     public ResponseEntity<String> deleteUser(@RequestParam int userId) {
-        // TODO: Return 200 OK if a user with the given ID exists, and the deletion is successful
-        //       Return 400 Bad Request if a user with the ID does not exist
-        //       The response body could be anything you consider appropriate
-        return null;
+        try {
+            // Check if user we are trying to delete exists before deleting
+            if (repository.existsById(userId)) {
+                repository.deleteById(userId);
+                return ResponseEntity.ok("Successfully deleted one user.");
+            } else {
+                return ResponseEntity
+                        .badRequest()
+                        .body("User with ID does not exist.");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
